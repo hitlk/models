c2e2dd52f684103005bfb4ee978850418830be94
li.ke <li.ke@navercorp.com>
2018-04-04 14:50:43 +0800

    #

--- research/object_detection/eval.py
+++ research/object_detection/eval.py
@@ -143,7 +143,8 @@ def main(unused_argv):
     eval_config.max_evals = 1
 
   evaluator.evaluate(create_input_dict_fn, model_fn, eval_config, categories,
-                     FLAGS.checkpoint_dir, FLAGS.eval_dir, FLAGS.eval_training_data)
+                     FLAGS.checkpoint_dir, FLAGS.eval_dir,
+                     data_aumentation=FLAGS.eval_training_data)
 
 
 if __name__ == '__main__':
